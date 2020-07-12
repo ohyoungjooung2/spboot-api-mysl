577fd7f12e32e3807b831e9e304d914a37ae1c85
oyj <oyj@local>
2020-07-12 23:48:05 +0900

    spboot-api-mysl first commit

---
+++ src/main/java/com/myqlapi/crudapi/ex/SpribootCrudApiMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.myqlapi.crudapi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpribootCrudApiMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpribootCrudApiMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myqlapi/crudapi/ex/SpribootCrudApiMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myqlapi.crudapi.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpribootCrudApiMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
